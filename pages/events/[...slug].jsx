9716cbfbfeb9da27fb6c26cd830f21819dea71bc
Mohammed Banani | MbDev <leedbn1@gmail.com>
2022-10-17 21:54:25 +0100

    final

--- pages/events/[...slug].jsx
+++ pages/events/[...slug].jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { useRouter } from 'next/router';
+import { getFilteredEvents } from '../../data';
+import EventList from '../../components/events/eventList';
+import ResultsTitle from '../../components/events/results-title';
 
 const FilteredEventsPage = () => {
-  return <div>FilteredEventsPage</div>;
+  const router = useRouter();
+  const filterData = router.query.slug;
+  if (!filterData) {
+    return <h1 className='center'>Loading...</h1>;
+  }
+  if (filterData.length !== 2) {
+    return <h1 className='center'>invalid url</h1>;
+  }
+  const filteredYear = +filterData[0];
+  const filteredMonth = +filterData[1];
+
+  if (isNaN(filteredMonth) || isNaN(filteredYear)) {
+    return <h1 className='center'>invalid url, please adjust your values</h1>;
+  }
+
+  const filteredEvents = getFilteredEvents({ year: filteredYear, month: filteredMonth });
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <h1 className='center'>No event was found</h1>;
+  }
+  const date = new Date(filteredYear, filteredMonth - 1);
+  return (
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
+  );
 };
 
 export default FilteredEventsPage;
--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import EventList from '../../components/events/eventList';
+import EventsSearch from '../../components/events/events-search';
 import { getAllEvents } from '../../data';
-
+import { useRouter } from 'next/router';
 const EventMainPage = () => {
   const events = getAllEvents();
+  const router = useRouter();
+  const findEventHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
+
   return (
-    <div>
+    <Fragment>
+      <EventsSearch onSearch={findEventHandler} />
       <EventList items={events} />
-    </div>
+    </Fragment>
   );
 };
 
